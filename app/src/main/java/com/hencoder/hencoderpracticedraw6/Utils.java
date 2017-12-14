c2c2af3aad0f87d0fa94bc637583801369ff4088
Your Name <you@example.com>
2017-12-14 21:51:58 +0800

    练习完成一半

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/Utils.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/Utils.java
@@ -4,6 +4,12 @@ import android.content.res.Resources;
 import android.util.DisplayMetrics;
 
 public class Utils {
+
+    /**
+     * dp转px
+     * @param dp 需要转换的dp值
+     * @return 转换后px的值
+     */
     public static float dpToPixel(float dp) {
         DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
         return dp * metrics.density;
